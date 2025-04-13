72a9fa49dff642a7cbe05e950fa78633f0f28819
ncotic <ncotic@abwfl.com>
2025-04-13 17:48:25 -0600

    update

--- core/src/main/java/com/abwfl/game/World.java
+++ core/src/main/java/com/abwfl/game/World.java
@@ -88,7 +88,7 @@ public class World {
         spaceTex = new Texture("space.png");
 
         try {
-            BufferedImage noiseBI = ImageIO.read(Gdx.files.internal("noise.png").file());
+            BufferedImage noiseBI = ImageIO.read(Gdx.files.internal("noise.png").read());
             int width = noiseBI.getWidth();
             int height = noiseBI.getHeight();
             ground = new float[height][width];
